bc5729bcb3ddbe7893b6001a18f6f3852c44df87
qihuo.wu <qihuo.wu@localhost>
2018-05-29 15:06:36 -0400

    529

--- src/main/java/sdt/proxy/socket/AcceptClientSocket.java
+++ src/main/java/sdt/proxy/socket/AcceptClientSocket.java
@@ -11,20 +11,20 @@ public class AcceptClientSocket {
          try {
              ServerSocket serverSocket = new ServerSocket(9000);
              System.out.println("代理服务器启动socket监听器完成...........");
-             long num=0;
+             Long num=(long)0;
+             Socket socket = serverSocket.accept();
              while(true) {
             	 System.out.println("线程池当前有"+ThreadManager.ThreadPool.getCompletedTaskCount()+"个任务完成........现在池里有"+ThreadManager.ThreadPool.getPoolSize()+"个线程....."+ThreadManager.ThreadPool.getActiveCount()+"个任务正在运行");
-            	
-            	 ThreadManager.ThreadPool.execute(
-            		    ThreadManager.excSocketThread.excMethod(
-            		    		  this,
-                   		        "startSocket",
-                   		        new Object[]{serverSocket.accept(),num++},
-                   		        Socket.class,
-                   		        long.class)
-            		     );
-                   
+            
+            	 ThreadManager.ThreadPool.execute(new Thread(()->{
+            		 try{
+            			 startSocket(socket,num); 
+            		 }catch (Exception e) {
+            			 e.printStackTrace();
+					}
+            	 }));
                  
+            	
                 }
             
              
@@ -33,12 +33,15 @@ public class AcceptClientSocket {
            }
      }
      
-     public void startSocket(Socket socket,long num){
+     public void startSocket(Socket socket,Long num){
     	 System.out.println("*****收到第"+num+"个请求");
-    	/// socket.set
+    	 num++;
     	 SocketHandle socketHandle = new SocketHandle(socket);
-    	 ThreadManager.ThreadPool.execute(ThreadManager.excSocketThread.excMethod(socketHandle,
-          		"run", new Object[]{}));
+    	/* ThreadManager.ThreadPool.execute(ThreadManager.excSocketThread.excMethod(socketHandle,
+          		"run", new Object[]{}));*/
+    	 ThreadManager.ThreadPool.execute(new Thread(()->{
+    		 socketHandle.run();
+    	 }));
      }
      
      
